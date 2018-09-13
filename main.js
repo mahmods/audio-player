3faccf0fe13d449a4ce892a4ef4ecfd4f25fec81
Mahmoud Slah <mahmodslah25@gmail.com>
2018-09-13 04:30:29 +0200

    style changes

--- main.js
+++ main.js
@@ -2,7 +2,10 @@
 audio = new Audio();
 playBtn        = document.getElementById('ap').querySelector('.ap__controls--toggle');
 playSvgPath    = document.querySelector('.icon-play').querySelector('path');
+curTime        = document.getElementById('ap').querySelector('.track__time--current');
+durTime        = document.getElementById('ap').querySelector('.track__time--duration');
 playBtn.addEventListener('click', playToggle, false);
+audio.addEventListener('timeupdate', timeUpdate, false);
 
 function playToggle() {
     if(audio.paused) {
@@ -24,11 +27,31 @@ function playToggle() {
       playSvgPath.setAttribute('d', document.querySelector('.icon-play').getAttribute('data-play'));
     }
   }
+  
+
+  function timeUpdate() {
+    var barlength = Math.round(audio.currentTime * (100 / audio.duration));
+
+    var
+    curMins = Math.floor(audio.currentTime / 60),
+    curSecs = Math.floor(audio.currentTime - curMins * 60),
+    mins = Math.floor(audio.duration / 60),
+    secs = Math.floor(audio.duration - mins * 60);
+    (curSecs < 10) && (curSecs = '0' + curSecs);
+    (secs < 10) && (secs = '0' + secs);
+
+    curTime.innerHTML = curMins + ':' + curSecs;
+    durTime.innerHTML = mins + ':' + secs;
+
+
+  }
 
   $('.container').on("click", function () {
     $('.track__title').html($(this).data('song'));
+    $('.track__artwork').attr('src', $(this).children('img').attr('src'));
     audio.src = $(this).data('song');
     playBtn.classList.add('is-playing');
     playSvgPath.setAttribute('d', document.querySelector('.icon-play').getAttribute('data-pause'));
     audio.play();
   })
+  
